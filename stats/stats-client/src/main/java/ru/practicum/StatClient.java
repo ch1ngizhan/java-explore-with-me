b3d805bb3fb0555cf058e8ff208f52aa11d59932
egopv <egop.valuev@yandex.ru>
2025-12-12 23:48:46 +0300

    mini fix

--- stats/stats-client/src/main/java/ru/practicum/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatClient.java
@@ -1,11 +1,12 @@
 package ru.practicum;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.StatDto;
 
@@ -14,7 +15,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
-@Service
+@Slf4j
+@Component
 public class StatClient extends BaseClient {
 
     public StatClient(@Value("${stats-service.url}") String serverUrl, RestTemplateBuilder builder) {
